b1e32daa15df9c9cc46ec6b5dda437bb1553cae0
Michael J Huber Jr <info@op131.com>
2017-01-08 13:08:56 -0700

    initialize repository

---
+++ Package.swift
@@ -0,0 +1,8 @@
+import PackageDescription
+
+let package = Package(
+    name: "op131",
+    dependencies: [
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 3)
+    ]
+)
---
+++ README.md
@@ -0,0 +1,2 @@
+# opertation thirteenOne's Website
+This website is built using the [Vapor](https://github.com/vapor) web framework.
