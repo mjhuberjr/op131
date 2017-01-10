3027003a41c9f6d6a2619796c89cb0df8c4b2324
Michael J Huber Jr <info@op131.com>
2017-01-09 20:33:37 -0700

    test DB with configuration

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # opertation thirteenOne's Website
 This website is built using the [Vapor](https://github.com/vapor) web framework.
+
+## Setup
+Being a pretty simple website that will not be taking a lot of daily changes, I've decided not to build this as an actual web app. All database entries are handled locally before being secure transfered to my server.
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,1 +1,16 @@
-print("Hello, world!")
+import Vapor
+import VaporMySQL
+
+let drop = Droplet()
+try drop.addProvider(VaporMySQL.Provider)
+
+drop.get("testdb") { _ in
+  if let db = drop.database?.driver as? MySQLDriver {
+    let version = try db.raw("SELECT version()")
+    return try JSON(node: version)
+  } else {
+    return "Database connection failed..."
+  }
+}
+
+drop.run()
