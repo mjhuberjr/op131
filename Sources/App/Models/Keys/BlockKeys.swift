8d067b7f76efc1bb42d45a3142e9a39e8c59d888
Michael J Huber Jr <info@op131.com>
2017-01-16 20:39:33 -0700

    implement BlockKeys

---
+++ Sources/App/Models/Keys/BlockKeys.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+enum BlockKeys: String {
+    case title
+    case subtitle
+    case description
+    case released
+    case image
+    case url
+    case links
+    case isPublished
+}
