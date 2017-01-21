db78c4684353f71cdad7685627a1041946c98a01
Michael J Huber Jr <info@op131.com>
2017-01-20 22:44:19 -0700

    test json with block and links based on section name

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -19,8 +19,15 @@ drop.get("testdb") { _ in
 drop.get(String.self) { request, sectionString in
     guard let section = try Section.query().filter("title", sectionString).all().first else { throw Abort.notFound }
     guard let sectionID = section.id, let block = try Block.find(sectionID) else { throw Abort.notFound }
+    let blocks = try section.children(nil, Block.self).all()
+    let blocksWithLinks = try blocks.flatMap { (block) -> JSON in
+        return try JSON(node: [
+            "block": block.makeNode(), "links": try block.children(nil, Link.self).all().makeNode()
+        ])
+    }
     
-    return try block.children(nil, Link.self).all().makeJSON()  //section.children(nil, Block.self).all().makeJSON()
+    
+    return try blocksWithLinks.makeJSON()  //section.children(nil, Block.self).all().makeJSON()
 }
 
 //drop.get(Section.self) { request, section in
