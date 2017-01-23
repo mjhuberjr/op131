149364ac50ceffae894a7b7ae6a0a42c8898102f
Michael J Huber Jr <info@op131.com>
2017-01-22 22:28:16 -0700

    implement blocks and links in SectionController

--- Sources/App/Controllers/SectionController.swift
+++ Sources/App/Controllers/SectionController.swift
@@ -8,7 +8,21 @@ final class SectionController {
     }
     
     func section(request: Request, sectionName: String) throws -> ResponseRepresentable {
-            guard let section = try Section.query().filter("title", sectionName).all().first else { throw Abort.notFound }
-            return try section.children(nil, Block.self).all().makeJSON()
+        guard let section = try Section.query().filter("title", sectionName).all().first else { throw Abort.notFound }
+        let blocks = try section.children(nil, Block.self).all().makeNode()
+        let links = try self.links(section: section).makeNode()
+        
+        let parameters = try Node(node: [
+            "blocks": blocks,
+            "links": links
+        ])
+        
+        return try drop.view.make("section", parameters)
+    }
+    
+    func links(section: Section) throws -> [Link] {
+        guard let sectionID = section.id, let block = try Block.find(sectionID) else { throw Abort.notFound }
+        let links = try block.children(nil, Link.self).all()
+        return links
     }
 }
