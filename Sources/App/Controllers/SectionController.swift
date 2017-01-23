5e4c238d80daf698bffa93bca8ae702810f53934
Michael J Huber Jr <info@op131.com>
2017-01-22 22:05:59 -0700

    add section route to SectionController based on name passed

--- Sources/App/Controllers/SectionController.swift
+++ Sources/App/Controllers/SectionController.swift
@@ -3,5 +3,12 @@ import HTTP
 import VaporMySQL
 
 final class SectionController {
+    func addRoutes(droplet: Droplet) {
+        drop.get(String.self, handler: section)
+    }
     
+    func section(request: Request, sectionName: String) throws -> ResponseRepresentable {
+            guard let section = try Section.query().filter("title", sectionName).all().first else { throw Abort.notFound }
+            return try section.children(nil, Block.self).all().makeJSON()
+    }
 }
--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -7,28 +7,31 @@ drop.preparations += Section.self
 drop.preparations += Block.self
 drop.preparations += Link.self
 
-drop.get("testdb") { _ in
-  if let db = drop.database?.driver as? MySQLDriver {
-    let version = try db.raw("SELECT version()")
-    return try JSON(node: version)
-  } else {
-    return "Database connection failed..."
-  }
-}
+let sectionController = SectionController()
+sectionController.addRoutes(droplet: drop)
 
-drop.get(String.self) { request, sectionString in
-    guard let section = try Section.query().filter("title", sectionString).all().first else { throw Abort.notFound }
-    guard let sectionID = section.id, let block = try Block.find(sectionID) else { throw Abort.notFound }
-    let blocks = try section.children(nil, Block.self).all()
-    let blocksWithLinks = try blocks.flatMap { (block) -> JSON in
-        return try JSON(node: [
-            "block": block.makeNode(), "links": try block.children(nil, Link.self).all().makeNode()
-        ])
-    }
-    
-    
-    return try blocksWithLinks.makeJSON()  //section.children(nil, Block.self).all().makeJSON()
-}
+//drop.get("testdb") { _ in
+//  if let db = drop.database?.driver as? MySQLDriver {
+//    let version = try db.raw("SELECT version()")
+//    return try JSON(node: version)
+//  } else {
+//    return "Database connection failed..."
+//  }
+//}
+
+//drop.get(String.self) { request, sectionString in
+//    guard let section = try Section.query().filter("title", sectionString).all().first else { throw Abort.notFound }
+//    guard let sectionID = section.id, let block = try Block.find(sectionID) else { throw Abort.notFound }
+//    let blocks = try section.children(nil, Block.self).all()
+//    let blocksWithLinks = try blocks.flatMap { (block) -> JSON in
+//        return try JSON(node: [
+//            "block": block.makeNode(), "links": try block.children(nil, Link.self).all().makeNode()
+//        ])
+//    }
+//    
+//    
+//    return try blocksWithLinks.makeJSON()  //section.children(nil, Block.self).all().makeJSON()
+//}
 
 //drop.get(Section.self) { request, section in
 //    return try section.children(nil, Block.self).all().makeJSON()
